ec34545fe297aec6d36844941443df1ba95964d4
zhangxiying <865747339@qq.com>
2019-03-15 15:54:46 +0800

    python2.7+django1.11 -3

--- apps/idcs/urls.py
+++ apps/idcs/urls.py
@@ -1,12 +1,23 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
-from .views import idc_list, idc_detail, idc_list_v2, idc_detail_v2
+# from .views import idc_list, idc_detail, idc_list_v2, idc_detail_v2
+from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+# urlpatterns = [
+#     url('^$', views.api_root),
+#     url('^idc/$', views.idc_list, name='idc-list'),
+#     url('^idc_v2/$', views.idc_list_v2),
+#     url('^idc/(?P<pk>[0-9]+)/$', views.idc_detail, name='idc-detail'),
+#     url('^idc_v2/(?P<pk>[0-9]+)/$', views.idc_detail_v2)
+#     # url('^idc', idc_list),
+# ]
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+##########################  版本三   ############################
 urlpatterns = [
-    url('^idc/$', idc_list),
-    url('^idc_v2/$', idc_list_v2),
-    url('^idc/(?P<pk>[0-9]+)/$', idc_detail),
-    url('^idc_v2/(?P<pk>[0-9]+)/$', idc_detail_v2)
-    # url('^idc', idc_list),
+    url("^$", views.api_root),
+    url("^idcs/$", views.IdcList.as_view(), name="idc-list"),
+    url("^idcs/(?P<pk>[0-9]+)/$", views.IdcDetail.as_view(), name="idc_detail")
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- apps/idcs/views.py
+++ apps/idcs/views.py
@@ -101,3 +101,66 @@ def idc_detail_v2(request, pk, *args, **kwargs):
     elif request.method == 'DELETE':
         idc.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(['GET'])
+# def api_root(request, *args, **kwargs):
+#     return Response({
+#         'idc': 'http://127.0.0.1:8000/idc_v2/'
+#     })
+
+
+from rest_framework.reverse import reverse
+@api_view(['GET'])
+def api_root(request, format=None, *args, **kwargs):
+    return Response({
+        'idc': reverse('idc-list', request=request, format=format)
+    })
+
+##########################  版本三   ############################
+from rest_framework.views import APIView
+from django.http import Http404
+
+
+class IdcList(APIView):
+    def get(self, request, format=None):
+        queryset = Idc.objects.all()
+        # print(queryset)
+        serializer = IdcSerializer(queryset, many=True)
+        # print(serializer)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = IdcSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class IdcDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return  Idc.objects.get(pk=pk)
+        except Idc.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        idc = self.get_object(pk)
+        print(idc)
+        serializer = IdcSerializer(idc)
+        print(serializer)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        idc = self.get_object(pk)
+        serializer = IdcSerializer(idc, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk, format=None):
+        idc = self.get_object(pk)
+        idc.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
